4449c581402ad2176fb629dab571864953842da9
Aleksei <dmt.alex2005@gmail.com>
2024-11-12 01:03:38 +0300

    add: formatter in printer func
    Signed-off-by: Aleksei <dmt.alex2005@gmail.com>

--- Go/tests/parser/qCheck/ppParseId.ml
+++ Go/tests/parser/qCheck/ppParseId.ml
@@ -10,15 +10,24 @@ open QCheck
 
 (* tmp *)
 let gen = gen_expr (gen_block gen_stmt)
-let print_file fmt = print_expr
+let print_file fmt = print_expr fmt
+
+let pprint_to_string input =
+  Format.fprintf Format.str_formatter "%a" print_expr input;
+  Format.flush_str_formatter ()
+;;
+
 let parse_file = Parse.parse (Expr.parse_expr Stmt.parse_block)
-let arbitrary_file = QCheck.make gen ~print:(Format.asprintf "%a" print_file)
+
+let arbitrary_file =
+  QCheck.make ~print:(fun ast -> Format.asprintf "%a" (print_file ast)) gen
+;;
 
 let run_tests () =
   QCheck_runner.run_tests
     [ QCheck.(
         Test.make ~count:10 arbitrary_file (fun file ->
-          Result.ok file = parse_file (Format.asprintf "%a" print_file file)))
+          file = parse_file (Format.asprintf "%a" print_file file)))
     ]
 ;;
 
