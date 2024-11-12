b5d60e4e9adfd6449d95a0779cda71adaafce5bb
Karim Shakirov <karimshak06@gmail.com>
2024-11-12 17:07:23 +0300

    test: fix qcheck
    + ref parser and repl
    
    Signed-off-by: Karim Shakirov <karimshak06@gmail.com>

---
+++ Go/tests/parser/qCheck/qCheckRun.ml
@@ -0,0 +1,27 @@
+(** Copyright 2024, Karim Shakirov, Alexei Dmitrievtsev *)
+
+(** SPDX-License-Identifier: MIT *)
+
+open QCheckTest.AstGenerator
+open QCheckTest.Pp
+open Parser
+
+(* tmp *)
+let gen = gen_expr (gen_block gen_stmt)
+let print_file = print_expr
+let parse_file = Parse.parse (Expr.parse_expr Stmt.parse_block)
+let arbitrary_file = QCheck.make gen ~print:(Format.asprintf "%a" print_file)
+
+let run_tests () =
+  QCheck_runner.run_tests
+    [ QCheck.(
+        Test.make ~name:"Parser QCheck test" ~count:10 arbitrary_file (fun file ->
+          Result.ok file = parse_file (Format.asprintf "%a" print_file file)))
+    ]
+;;
+
+let () =
+  let _ = print_endline "Running QCheck random generated tests" in
+  let _ = run_tests in
+  ()
+;;
